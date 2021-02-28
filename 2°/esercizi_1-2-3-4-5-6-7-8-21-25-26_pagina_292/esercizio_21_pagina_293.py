acc6de76c761e2d7891ce49945cc827a9804e2bc
Filippo-Ragni <filippo.ragni.2005@gmail.com>
2021-02-28 23:27:09 +0100

    add files esercizio_21_pagina_293.py and esercizio_36_pagina_293.py

--- 2°/esercizi_1-2-3-4-5-6-7-8-21-25-26_pagina_292/esercizio_21_pagina_293.py
+++ 2°/esercizi_1-2-3-4-5-6-7-8-21-25-26_pagina_292/esercizio_21_pagina_293.py
@@ -1,2 +1,61 @@
-from esercizio_7_pagina_292 import motorcycle
+class motorcycle:
 
+    def __init__(self, type, brand):
+        self.type = type
+        self.brand = brand
+        self.price = None
+
+    def give_price(self, price):
+        self.price = float(price)
+
+class minibike(motorcycle):
+    def __init__(self, type, brand, displacement, max_speed):
+        super().__init__(type, brand)
+        self.displacement = float(displacement)
+        self.max_speed = float(max_speed)
+
+    def print_data(self):
+        print("il ciclomotore è  un:", self.type)
+        print("è prodotto dall'azienda:", self.brand)
+        print("costa:", self.price, "euro")
+        print("ha una cilindrata di:", self.displacement, "cc")
+        print("può raggiungere una velocità massima di:", self.max_speed, "km/h")
+
+print("tipo ciclo motore: ")
+t = str(input())
+print()
+
+print("azienda produttrice: ")
+b = str(input())
+print()
+
+print("prezzo in euro: ")
+p = float(input())
+if p <= 0:
+    while p <= 0:
+        print("il prezzo del ciclo motore non può essere inferiore o uguale a 0")
+        print("prezzo in euro: ")
+        p = float(input())
+print()
+
+print("cilindrata in cc(centimetri cubici): ")
+m_d = float(input())
+if m_d <= 0 or m_d > 50:
+    while m_d <= 0 or m_d > 50:
+        print("la cilindrata del ciclomotore non può essere inferiore o uguale a 0 o superiore a 50 cc")
+        print("cilindrata in cc(centimetri cubici): ")
+        m_d = float(input())
+print()
+
+print("velocità massima in km/h(kilometri all'ora): ")
+m_s = float(input())
+if m_s <= 0 or m_s > 45:
+    while m_s <= 0 or m_s > 45:
+        print("la velocità massima del ciclo motore non può essere inferiore o uguale a 0 o maggire di 45 km/h")
+        print("velocità massima in km/h(kilometri all'ora): ")
+        m_s = float(input())
+print()
+
+cm = minibike(t, b, m_d, m_s)
+cm.give_price(p)
+cm.print_data()
--- 2°/esercizi_1-2-3-4-5-6-7-8-21-25-26_pagina_292/esercizio_26_pagina_293.py
+++ 2°/esercizi_1-2-3-4-5-6-7-8-21-25-26_pagina_292/esercizio_26_pagina_293.py
@@ -1,2 +1,47 @@
-from esercizio_8_pagina_292 import square
+class square:
 
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        self.area = self.side ** 2
+
+    def perimeter(self):
+        self.perimeter = self.side * 4
+
+class rectangol(square):
+    def __init__(self, side, height):
+        super().__init__(side)
+        self.height = height
+
+    def area_and_perimeter(self):
+        self.area = self.height * self.side
+        self.perimeter = (self.side * 2) + (self.height * 2)
+    
+    def print_data(self):
+        print("il rettangolo di lato:", self.side)
+        print("e di altezza:", self.height)
+        print("ha area di:", self.area, "metri quadrati")
+        print("e perimetro di:", self.perimeter, "metri")
+
+print("lunghezza lato in metri: ")
+l_side = float(input())
+if l_side <= 0:
+    while l_side <= 0:
+        print("il lato del rettangolo non può essere inferiore o ugualea 0")
+        print("lunghezza lato in metri: ")
+        l_side = float(input())
+print()
+
+print("lunghezza altezza in metri: ")
+l_height = float(input())
+if l_height <= 0:
+    while l_height <= 0:
+        print("l'altezza del rettangolo non può essere inferiore o ugualea 0")
+        print("lunghezza lato in metri: ")
+        l_height = float(input())
+print()
+
+r = rectangol(l_side, l_height)
+r.area_and_perimeter()
+r.print_data()
