553cf5efc5e9041436b3669b5ff836ee2aa13637
Filippo-Ragni <filippo.ragni.2005@gmail.com>
2021-03-17 21:54:12 +0100

    add inizio partita to scacchi_strani.py

--- 2°/roba_Bruno/scacchi_strani.py
+++ 2°/roba_Bruno/scacchi_strani.py
@@ -1,16 +1,17 @@
 # classi pedine e scacchiera
 
 class piece:
-    def __init__(self, x, y, colour, board): 
+    def __init__(self, x, y, colour, board, pi): 
         self.x = x
         self.y = y
+        self.pi = pi
         self.colour  = colour
         self.board = board
         self.board.pieces.append(self) 
 
 class king(piece):
-    def __init__(self, x, y, board, pi):
-        super().__init__(x, y, colour, board)
+    def __init__(self, x, y, colour, board, pi):
+        super().__init__(x, y, colour, board, pi)
 
     def move(self):
         x = int(input("x: "))
@@ -35,8 +36,8 @@ class king(piece):
             self.y = y
 
 class queen(piece):
-    def __init__(self, x, y, board, pi):
-        super().__init__(x, y, colour, board)
+    def __init__(self, x, y, colour, board, pi):
+        super().__init__(x, y, colour, board, pi)
 
     def move(self):
         x = int(input("x: "))
@@ -58,66 +59,33 @@ class queen(piece):
             print("non puoi muovere la regina in questa cella")
 
 class rook(piece):
-    def __init__(self, x, y, board, pi):
-        super().__init__(x, y, colour, board)
+    def __init__(self, x, y, colour, board, pi):
+        super().__init__(x, y, colour, board, pi)
 
 class bishop(piece):
-    def __init__(self, x, y, board, pi):
-        super().__init__(x, y, colour, board)
+    def __init__(self, x, y, colour, board, pi):
+        super().__init__(x, y, colour, board, pi)
 
 class knight(piece):
-    def __init__(self, x, y, board, pi):
-        super().__init__(x, y, colour, board)
+    def __init__(self, x, y, colour, board, pi):
+        super().__init__(x, y, colour, board, pi)
 
 class pond(piece):
-    def __init__(self, x, y, board, pi):
-        super().__init__(x, y, colour, board)
+    def __init__(self, x, y, colour, board, pi):
+        super().__init__(x, y, colour, board, pi)
 
 class board:
     def __init__(self):
         self.w = 8
         self.h = 8
-        self.entities = []
+        self.pieces = []
 
     def draw(self):
         for y in range(self.h):
             for x in range(self.w):
-                for e in self.entities:
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "wq":
-                        print("[wq]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "wk":
-                        print("[wk]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "wb":
-                        print("[wb]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "wr":
-                        print("[wr]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "wh":
-                        print("[wh]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "wp":
-                        print("[wp]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "bq":
-                        print("[bq]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "bk":
-                        print("[bk]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "bb":
-                        print("[bb]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "br":
-                        print("[br]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "bh":
-                        print("[bh]", end = "")
-                        break
-                    if x + 1 == e.x and y + 1 == e.y and e.pi == "bp":
-                        print("[bp]", end = "")
+                for e in self.pieces:
+                    if x + 1 == e.x and y + 1 == e.y:
+                        print("[" + e.colour + e.pi + "]", end = "")
                         break
                 else: 
                     print("[  ]", end = "")
@@ -129,38 +97,38 @@ class board:
 
 c_board = board()
 
-wk = king(5, 8, "w", c_board)
-wq = queen(4, 8, "w", c_board)
-wr1 = rook(1, 8, "w", c_board)
-wr2 = rook(8, 8, "w", c_board)
-wb1 = bishop(3, 8, "w", c_board)
-wb2 = bishop(6, 8, "w", c_board)
-wh1 = knight(2, 8, "w", c_board)
-wh2 = knight(7, 8, "w", c_board)
-wp1 = pond(1, 7, "w", c_board)
-wp2 = pond(2, 7, "w", c_board)
-wp3 = pond(3, 7, "w", c_board)
-wp4 = pond(4, 7, "w", c_board)
-wp5 = pond(5, 7, "w", c_board)
-wp6 = pond(6, 7, "w", c_board)
-wp7 = pond(7, 7, "w", c_board)
-wp8 = pond(8, 7, "w", c_board)
+wk = king(5, 8, "w", c_board, "k")
+wq = queen(4, 8, "w", c_board, "q")
+wr1 = rook(1, 8, "w", c_board, "r")
+wr2 = rook(8, 8, "w", c_board, "r")
+wb1 = bishop(3, 8, "w", c_board, "b")
+wb2 = bishop(6, 8, "w", c_board, "b")
+wh1 = knight(2, 8, "w", c_board, "h")
+wh2 = knight(7, 8, "w", c_board, "h")
+wp1 = pond(1, 7, "w", c_board, "p")
+wp2 = pond(2, 7, "w", c_board, "p")
+wp3 = pond(3, 7, "w", c_board, "p")
+wp4 = pond(4, 7, "w", c_board, "p")
+wp5 = pond(5, 7, "w", c_board, "p")
+wp6 = pond(6, 7, "w", c_board, "p")
+wp7 = pond(7, 7, "w", c_board, "p")
+wp8 = pond(8, 7, "w", c_board, "p")
 
-bk = king(4, 1, "b", c_board)
-bq = queen(5, 1, "b", c_board)
-br1 = rook(1, 1, "b", c_board)
-br2 = rook(8, 1, "b", c_board)
-bb1 = bishop(3, 1, "b", c_board)
-bb2 = bishop(6, 1, "b", c_board)
-bh1 = knight(2, 1, "b", c_board)
-bh2 = knight(7, 1, "b", c_board)
-bp1 = pond(1, 2, "b", c_board)
-bp2 = pond(2, 2, "b", c_board)
-bp3 = pond(3, 2, "b", c_board)
-bp4 = pond(4, 2, "b", c_board)
-bp5 = pond(5, 2, "b", c_board)
-bp6 = pond(6, 2, "b", c_board)
-bp7 = pond(7, 2, "b", c_board)
-bp8 = pond(8, 2, "b", c_board)
+bk = king(4, 1, "b", c_board, "k")
+bq = queen(5, 1, "b", c_board, "q")
+br1 = rook(1, 1, "b", c_board, "r")
+br2 = rook(8, 1, "b", c_board, "r")
+bb1 = bishop(3, 1, "b", c_board, "b")
+bb2 = bishop(6, 1, "b", c_board, "b")
+bh1 = knight(2, 1, "b", c_board, "h")
+bh2 = knight(7, 1, "b", c_board, "h")
+bp1 = pond(1, 2, "b", c_board, "p")
+bp2 = pond(2, 2, "b", c_board, "p")
+bp3 = pond(3, 2, "b", c_board, "p")
+bp4 = pond(4, 2, "b", c_board, "p")
+bp5 = pond(5, 2, "b", c_board, "p")
+bp6 = pond(6, 2, "b", c_board, "p")
+bp7 = pond(7, 2, "b", c_board, "p")
+bp8 = pond(8, 2, "b", c_board, "p")
 
 c_board.draw()
